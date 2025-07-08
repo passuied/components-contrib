4879c3996cbb2aef01640d52caf68827933af288
Patrick Assuied <patrick.assuied@elationhealth.com>
2025-07-07 23:56:10 -0700

    rolled back small change
    Signed-off-by: Patrick Assuied <patrick.assuied@elationhealth.com>

--- secretstores/aws/secretmanager/secretmanager.go
+++ secretstores/aws/secretmanager/secretmanager.go
@@ -209,5 +209,8 @@ func (s *smSecretStore) GetComponentMetadata() (metadataInfo metadata.MetadataMa
 }
 
 func (s *smSecretStore) Close() error {
-	return s.authProvider.Close()
+	if s.authProvider != nil {
+		return s.authProvider.Close()
+	}
+	return nil
 }
